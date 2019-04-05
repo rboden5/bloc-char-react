0e9cbf5043d8024fec4cfd2be855286b2d09db32
Your Name <ryanboden5@gmail.com>
2019-04-05 09:03:59 -0700

    list chat rooms done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+var config = {
+  apiKey: "AIzaSyCsguLLm5AlXcEPYD-_5B1b5N_KsZq0Q40",
+  authDomain: "bloc-chat-react-3a4fb.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-3a4fb.firebaseio.com",
+  projectId: "bloc-chat-react-3a4fb",
+  storageBucket: "bloc-chat-react-3a4fb.appspot.com",
+  messagingSenderId: "536533688821"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super()
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1> Bloc Chat</h1>
         </header>
+        <main>
+          <div>
+          <RoomList firebase={firebase}/>
+          </div>
+        </main>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state ={
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+componentDidMount(){
+  this.roomsRef.on('child_added', snapshot => {
+    const room = snapshot.val();
+    room.key = snapshot.key;
+    this.setState({ rooms: this.state.rooms.concat( room ) })
+  });
+}
+
+  render () {
+    return (
+      <section className = "room-list-wrapper">
+        <ul className="chat-rooms">
+          {this.state.rooms.map((room, index) =>
+            <li className = "room-names" key={index}>{room.name}</li>)}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default RoomList
