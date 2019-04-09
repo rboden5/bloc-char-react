ab844b740a5443e4bbbca1d23f8d0686927bae12
Your Name <ryanboden5@gmail.com>
2019-04-08 21:51:35 -0700

    create chat room done

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -9,7 +9,22 @@ class RoomList extends Component {
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.createRoom = this.createRoom.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+handleChange(e){
+  this.setState({name: e.target.value});
+}
+
+
+createRoom(e){
+  e.preventDefault();
+  let newRoomName = this.state.name;
+  this.roomsRef.push({
+    name: newRoomName
+  });
+}
 
 componentDidMount(){
   this.roomsRef.on('child_added', snapshot => {
@@ -26,6 +41,17 @@ componentDidMount(){
           {this.state.rooms.map((room, index) =>
             <li className = "room-names" key={index}>{room.name}</li>)}
         </ul>
+        <form className = "create-new-room" onSubmit={this.createRoom}>
+          <label>Enter New Room Name:</label><br/>
+          <input
+            id = "submitNewRoom"
+            type="text"
+            name="name"
+            placeholder="Room Name"
+            value={this.state.name}
+            onChange={e => this.handleChange(e)}/><br/>
+          <button id='submitForm'>Submit</button><br/>
+        </form>
       </section>
     );
   }
