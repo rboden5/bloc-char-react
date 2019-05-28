01f2965cebfe3c7b972ab200b16d6d5912ac0bf6
Your Name <ryanboden5@gmail.com>
2019-05-28 15:21:31 -0700

    send messages done

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ class App extends Component {
                <MessageList
                  firebase = {firebase}
                  activeRoom = {this.state.activeRoom}
+                 user = {this.state.user}
                />
              )}
              </div>
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -6,10 +6,14 @@ class MessageList extends Component {
 
     this.state ={
       /*Init state with empty array so we can push data to it later*/
-      messages: []
+      messages: [],
+      newMessage: ''
     }
 /*References messages table in firebase*/
   this.messagesRef = this.props.firebase.database().ref('Messages');
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmitMessage = this.handleSubmitMessage.bind(this);
+
   }
 
 /*Event to grab messages from firebase*/
@@ -21,15 +25,48 @@ componentDidMount(){
     });
   }
 
+handleChange (e){
+  this.setState({newMessage: e.target.value});
+console.log(this.state.newMessage);
+}
+
+handleSubmitMessage(e){
+  e.preventDefault();
+
+  this.messagesRef.push({
+    content: this.state.newMessage,
+    username: this.props.user ? this.props.user.displayName : "Guest",
+    sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
+    roomId: this.props.activeRoom.key
+  });
+  this.setState({newMessage:''});
+}
+
+
+
 render() {
   return(
     <div className = "message-list">
       <div>
-      {this.state.messages.filter(message =>
-        message.roomID == this.props.activeRoom.key).map((message, index) =>
-        <p>{message.content}{"  "}
-        {message.sentAt}{"  "}
-        {message.username}</p>)}
+        {this.state.messages.filter(message =>
+          message.roomId == this.props.activeRoom.key).map((message, index) =>
+          <div className = "single-message">
+            <div className = "username-time">
+              <div id = "m-username">{message.username}</div>
+            </div>
+            <div id = "m-content">{message.content}</div>
+          </div>
+          )}
+      </div>
+      <div>
+        <form className = "send-message-form" onSubmit = {this.handleSubmitMessage}>
+          <input className="message-input"
+            onChange={e => this.handleChange(e)}
+            value = {this.state.newMessage}
+            placeholder = "Type your message and hit ENTER"
+            type = "text"/>
+            <input type ="submit"/>
+        </form>
       </div>
     </div>
   )
